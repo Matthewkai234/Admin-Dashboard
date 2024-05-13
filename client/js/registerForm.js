02c859a8892c891be773346964b95cd087b0cbf5
Saif Khalifa <63987779+SaifKhalifa@users.noreply.github.com>
2024-05-13 23:44:04 +0300

    Completed main task; design improvements needed

--- client/js/registerForm.js
+++ client/js/registerForm.js
@@ -9,106 +9,113 @@ let emailField = document.getElementById("inputEmail");
 let registerButton = document.getElementById("RegButton");
 let errorMsgs = document.getElementsByClassName("error-msg");
 
-import insertData from '../../server/connector_mongodb';
 //------------------------------ FUNCTIONS ------------------------------
-function checkPassword(passwordValue)
-{
-    /*
-        Validation Rules:
-            Value must have: 
-            1.capital letters AND small letters 
-            2.numbers.
-            3.special characters.
-            4.length between 8 and 16 digits.
-        Validation Error Message:
-            If the pattern is incorrect: 
-                "Password must contain capital letters, small letters, numbers, and any special character and length between 8 and 16 digits."
-    */
-   let flag = 0;
-   let isThereSmall = /[a-z]/;
-   let isThereCapital = /[A-Z]/;
-   let isThereNumber = /[0-9]/;
-   let isThereSpecialChar = /[^\w\s]/;
-
-   if(!(passwordValue.length >= 8 && passwordValue.length <= 16))
+function regButtonError(valid) {
+    if(!valid)
     {
-        flag++;
+        registerButton.style.backgroundColor = "#D32F2F";
+
+        registerButton.textContent = "Oopsie whoopsie!, there's something wrong in the form";
+
+        setTimeout(() => {
+            registerButton.style.backgroundColor = "#0d6efd"; // Reset to default background color
+            registerButton.textContent = "Create Account"; // Reset to default text
+        }, 3000); // 3000 milliseconds = 3 seconds
     }
-   else
-   {
-        if(!isThereSmall.test(passwordValue))
-            flag++;
-        if(!isThereCapital.test(passwordValue))
-            flag++;
-        if(!isThereNumber.test(passwordValue))
-            flag++;
-        if(!isThereSpecialChar.test(passwordValue))
-            flag++;
+    else
+    {
+        registerButton.style.backgroundColor = "#4CAF50";
+        registerButton.textContent = "Success!";
+    }
+    
+}
+
+function checkPassword(passwordValue) {
+    /*
+          Validation Rules:
+              Value must have: 
+              1.capital letters AND small letters 
+              2.numbers.
+              3.special characters.
+              4.length between 8 and 16 digits.
+          Validation Error Message:
+              If the pattern is incorrect: 
+                  "Password must contain capital letters, small letters, numbers, and any special character and length between 8 and 16 digits."
+      */
+    let flag = 0;
+    let isThereSmall = /[a-z]/;
+    let isThereCapital = /[A-Z]/;
+    let isThereNumber = /[0-9]/;
+    let isThereSpecialChar = /[^\w\s]/;
+
+    if (!(passwordValue.length >= 8 && passwordValue.length <= 16)) {
+        flag++;
+    } else {
+        if (!isThereSmall.test(passwordValue)) flag++;
+        if (!isThereCapital.test(passwordValue)) flag++;
+        if (!isThereNumber.test(passwordValue)) flag++;
+        if (!isThereSpecialChar.test(passwordValue)) flag++;
     }
 
     return !(flag > 0);
 }
 
-function formValidation()
-{
+function formValidation() {
     let isValid = 0;
     //-------------------------------- PASSWORD FIELD --------------------------------
-    if(!checkPassword(passwordField.value))
-    {
+    if (!checkPassword(passwordField.value)) {
         document.getElementById("passwordHelpBlock").style.display = "none";
-        errorMsgs[0].textContent = "Password must contain capital letters, small letters, numbers, and special character and length between 8 and 16 digits.";
+        errorMsgs[0].textContent =
+            "Password must contain capital letters, small letters, numbers, and special character and length between 8 and 16 digits.";
         isValid++;
-    }
-    else
-        errorMsgs[0].textContent = "";
+    } else errorMsgs[0].textContent = "";
 
     //-------------------------------- CONFIRM PASSWORD FIELD --------------------------------
-    if(confirmPasswordField.value !== passwordField.value)
-    {
+    if (confirmPasswordField.value !== passwordField.value) {
         errorMsgs[1].textContent = "Passwords doesn't match!";
         isValid++;
-    }
-    else
-        errorMsgs[1].textContent = "";
+    } else errorMsgs[1].textContent = "";
     //-------------------------------- NAME & EMAIL FIELD --------------------------------
-    if (firstNameField.value == '') {
-        isValid++;
-    }
-    
-    if (lastNameField.value == '') {
+    if (firstNameField.value == "") {
         isValid++;
     }
 
-    if (emailField.value == '') {
+    if (lastNameField.value == "") {
+        isValid++;
+    }
+
+    if (emailField.value == "") {
         isValid++;
     }
     //-------------------------------- RESULT --------------------------------
-    if(isValid == 0)// Register the user?
-    {
-        alert("User registered!");
-
-        const data =
-        {
+    if (isValid == 0) {
+        // Register the user?
+        //alert("User registered!");
+        regButtonError(true);
+        const data = {
             firstName: firstNameField.value,
             lastName: lastNameField.value,
             email: emailField.value,
             password: passwordField.value,
-            createdAt: new Date()
+            createdAt: new Date(),
         };
 
         insertData(data)
-        .then(() => {
-            console.log("Data inserted successfully!");
-        })
-        .catch((error) => {
-            console.error("Error inserting data:", error);
-        });
+            .then(() => {
+                console.log("Data inserted successfully!");
+            })
+            .catch((error) => {
+                console.error("Error inserting data:", error);
+            });
+    } 
+    else 
+    {
+        //alert("Oops, form not valid!");
+        regButtonError(false);
     }
-    else
-        alert("Oops, form not valid!");
 }
 //--------------------------- EVENT LISTENERS ---------------------------
-passwordField.addEventListener("input", function (){
+passwordField.addEventListener("input", function() {
     errorMsgs[0].textContent = "";
     errorMsgs[1].textContent = "";
     document.getElementById("passwordHelpBlock").style.display = "block";
--- server/connector_mongodb.js
+++ server/connector_mongodb.js
@@ -1,26 +1,85 @@
 const { MongoClient } = require("mongodb");
 
 // Replace the uri string with your connection string.
-const uri = "mongodb+srv://khsaif5:<password>.jryohb3.mongodb.net/?retryWrites=true&w=majority&appName=adminDB";
+const uri =
+  "mongodb+srv://khsaif5:8mgeD8nEWuhz9gD0@admindb.jryohb3.mongodb.net/?retryWrites=true&w=majority&appName=adminDB";
 
 const client = new MongoClient(uri);
 
-async function run(data) {
-  try {
-    await client.connect();
-    const database = client.db('web-2-db');
-    const users = database.collection('users');
+async function run() {
+  await client.connect();
+}
 
-    if(data !== null)
-    {
-        const result = await users.insertOne(data);
-    }
+async function insertUser(
+  firstName,
+  lastName,
+  email,
+  password,
+  confirmPassword,
+) {
+  const database = client.db("web-2-db");
+  const users = database.collection("users");
+  if (password != confirmPassword) {
+    throw "passwords do not match";
+  }
 
-    //console.log(`A document was inserted with the _id: ${result.insertedId}`);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+  if (!validatePassword(password)) {
+    throw "invalid password";
+  }
+
+  const validateEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  if (!validateEmail.test(email)) {
+    throw "invalid email";
+  }
+
+  const doesEmailExist = await users.findOne({ email });
+  if (doesEmailExist) {
+    throw "email already exists";
+  }
+  await users.insertOne({
+    firstName,
+    lastName,
+    email,
+    password
+    //createdAt : new Date() 
+  });
+
+  //console.log(`A document was inserted with the _id: ${result.insertedId}`);
+}
+
+async function loginUser(email, password) {
+  const database = client.db("web-2-db");
+  const users = database.collection("users");
+
+  const dbUser = await users.findOne({ email });
+
+  if (!dbUser) {
+    throw "email does not exist";
+  }
+
+  if (dbUser.password !== password) {
+    throw "invalid email or password";
   }
 }
 
-module.exports = run;
+function validatePassword(password) {
+  if (password.length < 8 || password.length > 16) {
+    return false;
+  }
+  const capitalRegex = /[A-Z]/;
+  const smallRegex = /[a-z]/;
+  const numberRegex = /[0-9]/;
+  const symbolRegex = /[@!_$%#]/;
+
+  if (
+    !capitalRegex.test(password) ||
+    !smallRegex.test(password) ||
+    !numberRegex.test(password) ||
+    !symbolRegex.test(password)
+  ) {
+    return false;
+  }
+  return true;
+}
+
+module.exports = { run, insertUser, loginUser };
